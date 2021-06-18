69d77602d1c448f3fe2a71e62f807ccc0dcf08e2
MickiRhen <mickirhen@gmail.com>
2021-06-18 12:50:50 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rhenhw3',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    version='0.1.0',
+    description='MIS 6900 aap course - Homework 3 - Simulation',
+    author='Michelle Rhen',
+    license='',
+)
